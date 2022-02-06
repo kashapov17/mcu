1e19e014da93872c3d4222049ac844b371340c88
kashapovd <y.kashapovd@gmail.com>
2022-02-06 18:18:00 +0300

    added work1

---
+++ work1/work1/main.asm
@@ -0,0 +1,46 @@
+;
+; work1.asm
+;
+; Created: 05.02.2022 7:38:24
+; Author : Yaroslav
+; Task: var6 F=2*(A-B)+C, A=68, B=43, C=129, D=149, E=4, G=5
+;
+
+.def F=r28
+.def A=r31
+.def B=r30
+.def C=r29
+.def D=r27
+.def I=r26
+.def E=r25
+.def R=r20
+.equ G=0x05
+
+ldi A,68
+ldi B,43
+ldi C,129
+ldi D,149
+ldi E,4
+
+calcF:
+	mov F,A
+	sub F,B
+	add F,F
+	add F,C
+compDF:
+	ldi I,12
+	cp D,F
+	brlo setI
+calcE:
+	mul E,I
+calcR:
+	ldi R,0
+	sbrs r0,G
+	ldi R,0xFF
+end:
+	nop
+	rjmp end
+
+setI:
+	ldi I,9
+	rjmp calcE
