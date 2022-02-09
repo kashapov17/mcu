7da46404791922e1d70cd4ccbb533dc94bad9652
kashapovd <y.kashapovd@gmail.com>
2022-02-09 21:27:55 +0300

    implemented sorting algo

--- work2/work2/main.asm
+++ work2/work2/main.asm
@@ -28,6 +28,9 @@ arr_B_raw: .db 6,1,8,9,1,0,3,5
 	.def rsize=r22
 	.def ridx=r23
 	.def ritem=r24
+	push rsize
+	push ridx
+	push ritem
 	ldi ZL, LOW(@0)
 	ldi ZH, HIGH(@0)
 	ldi XL, LOW(@1)
@@ -40,7 +43,10 @@ load_loop:
 	st X+, ritem
 	dec ridx
 	brne load_loop
-end:
+load_arr_to_mem_end:
+	pop ritem
+	pop ridx
+	pop rsize
 	.undef rsize
 	.undef ridx
 	.undef ritem
@@ -51,6 +57,10 @@ end:
 	.def rmax=r23
 	.def ridx=r24
 	.def ritem=r25
+	push rsize
+	push rmax
+	push ridx
+	push ritem
 	ldi ZL, LOW(@0)
 	ldi ZH, HIGH(@0)
 	ldi XL, LOW(@2)
@@ -63,24 +73,112 @@ find_loop:
 	ld ritem, Z+
 	cp rmax, ritem
 	brlo set_max
+	rjmp after_max_setting
+set_max:
+	mov rmax,ritem
 after_max_setting:
 	dec ridx
 	brne find_loop
-	rjmp end
-set_max:
-	mov rmax,ritem
-	rjmp after_max_setting
-end:
+	rjmp max_from_array_end
+max_from_array_end:
 	st X, rmax
+	push rsize
+	push rmax
+	push ridx
+	push ritem
 	.undef rsize
 	.undef rmax
 	.undef ridx
 	.undef ritem
-.endm	
+.endm
+
+; insertion or selection sort (I don't know :) I just got it from my mind)
+; (descending mode). best O(n) avg and worst O(n^2)
+.macro SORT_ARRAY
+	.def rkeyj=r22
+	.def rkeyi=r21
+	.def rsize=r23
+	.def ridx=r24
+	.def rjdx=r25
+	push rkeyj
+	push rkeyi
+	push rsize
+	push ridx
+	push rjdx
+	ldi ZL, LOW(@0)
+	ldi ZH, HIGH(@0)
+	ldi ridx, 0
+	ldi rsize, @1
+element_loop:
+	mov rjdx, ridx
+	inc rjdx
+	cp ridx, rsize
+	breq sort_end
+	mov YH, ZH
+	mov YL, ZL
+	ld rkeyi, Z+
+	mov XH, ZH
+	mov XL, ZL
+	ld rkeyj, X
+swap_loop:
+	cp rjdx, rsize
+	breq swap_loop_end
+	cp rkeyj, rkeyi
+	cpse rkeyj, rkeyi
+	brge swap_stage 
+	rjmp swap_loop_postroutine
+swap_stage:
+	swp Y, X
+	ld rkeyi, Y
+swap_loop_postroutine:
+	adiw XH:XL, 1
+	ld rkeyj, X
+	inc rjdx
+	rjmp swap_loop
+swap_loop_end:
+	inc ridx
+	rjmp element_loop
+sort_end:
+	pop rkeyj
+	pop rkeyi
+	pop rsize
+	pop ridx
+	pop rjdx
+	.undef rkeyi
+	.undef rkeyj
+	.undef rsize
+	.undef ridx
+	.undef rjdx	
+.endm
+
+;swap two value in memory space
+.macro swp
+	.def rvar1=r22
+	.def rvar2=r23
+	push rvar1
+	push rvar2
+	ld rvar1, @0
+	ld rvar2, @1
+	st @1, rvar1
+	st @0, rvar2
+	pop rvar2
+	pop rvar1
+	.undef rvar1
+	.undef rvar2
+.endm
+
+init_stack:
+	ldi r16, LOW(RAMEND)
+	out SPL, r16
+	ldi r16, HIGH(RAMEND)
+	out SPH, r16
 
 load_arrays:
 	LOAD_ARR_TO_MEM arr_A_raw, arr_A_mem, A_size
 	LOAD_ARR_TO_MEM arr_B_raw, arr_B_mem, B_size
+
+perform_task:
 	MAX_FROM_ARRAY arr_B_mem, B_size, item
-	nop
-	nop
+	SORT_ARRAY arr_A_mem, A_size
+	SORT_ARRAY arr_B_mem, B_size
+
